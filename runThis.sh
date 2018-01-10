1b52bc05c35ff5e2d1363557e29c66a788a668fc
Philip Mire <pmire@atlassian.com>
2018-01-10 12:26:51 -0600

    adding run script

---
+++ runThis.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+OSQI=/usr/bin/osqueryi
+
+gcc chkThis.cpp -o chkThis
+gcc doThis.c -o doThis
+
+./doThis &
+doThis_PID=$!
+
+sleep 2
+
+echo 
+$OSQI --version
+
+echo
+echo "================================"
+echo "Checking with executable PRESENT"
+./chkThis $doThis_PID
+$OSQI "select pid, path, cwd, cmdline, on_disk from processes where pid=$doThis_PID;"
+
+rm ./doThis
+
+echo 
+echo "================================"
+echo "Checking with executable DELETED"
+./chkThis $doThis_PID
+$OSQI "select pid, path, cwd, cmdline, on_disk from processes where pid=$doThis_PID;"
+
+echo
+
+kill $doThis_PID 
+rm chkThis
